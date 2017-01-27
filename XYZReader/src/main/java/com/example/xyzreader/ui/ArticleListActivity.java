a659b9c9b406d0040a7a950757fc566b04d07d20
Prashant Panwar <prashant.panwar777@gmail.com>
2017-01-28 02:28:58 +0530

    fix: article list activity abstrat method

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -29,7 +29,7 @@ import com.example.xyzreader.data.UpdaterService;
  * touched, lead to a {@link ArticleDetailActivity} representing item details. On tablets, the
  * activity presents a grid of items as cards.
  */
-public abstract class ArticleListActivity extends ActionBarActivity implements
+public class ArticleListActivity extends ActionBarActivity implements
         LoaderManager.LoaderCallbacks<Cursor>, SwipeRefreshLayout.OnRefreshListener {
 
     //private Toolbar mToolbar;
